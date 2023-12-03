88bfade368e2b880e4b954bc3e113509726528f5
Hajarita12 <120518556+Hajarita12@users.noreply.github.com>
2023-12-03 14:47:12 +0100

    Commit
    03-12-2023

---
+++ app/src/main/java/com/mourid/employeeapp/api/RetrofitEmploye.java
@@ -0,0 +1,17 @@
+package com.mourid.employeeapp.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitEmploye {
+    private static final String BASE_URL = "http://10.0.2.2:2020/api/employees/";
+
+    private static Retrofit retrofit;
+
+    public static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        };
+        return  retrofit;
+    }
+}
